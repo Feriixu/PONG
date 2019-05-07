7a54914be125ce58dc94f41a3d1d70fe05744d5b
Feriixu <feriixu@users.noreply.github.com>
2019-05-07 19:30:59 +0200

    C# Only Projekt Hinzugefügt

---
+++ PONG/Game.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace PONG
+{
+    partial class Game
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gameArena = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.gameArena)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // gameArena
+            // 
+            this.gameArena.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.gameArena.Location = new System.Drawing.Point(0, 0);
+            this.gameArena.Name = "gameArena";
+            this.gameArena.Size = new System.Drawing.Size(584, 561);
+            this.gameArena.TabIndex = 0;
+            this.gameArena.TabStop = false;
+            // 
+            // Game
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 561);
+            this.Controls.Add(this.gameArena);
+            this.Name = "Game";
+            this.Text = "PONG!";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.gameArena)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox gameArena;
+    }
+}
+
---
+++ PONG/Game.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace PONG
+{
+    public partial class Game : Form
+    {
+        System.Timers.Timer timer1, timer2;
+
+        Graphics Draw;
+        SolidBrush sb = new SolidBrush(Color.White);
+        public Game()
+        {
+            InitializeComponent();
+            gameArena.BackColor = Color.Black;
+            Draw = gameArena.CreateGraphics();
+
+            timer1 = new System.Timers.Timer() { Interval = 10 };
+            timer2 = new System.Timers.Timer() { Interval = 3000 };
+
+            timer1.Elapsed += timer1_Tick;
+            timer2.Elapsed += timer2_Tick;
+
+            timer1.Start();
+            timer2.Start();
+        }
+
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            PongGame.DrawIt(Draw);                      //Draws paddles & ball
+            PongGame.MoveBall(timer1);                  //Moves the ball
+            PongGame.CheckScore();                      //Check if one player scored
+            PongGame.CheckIfMoving();                   //Method that check if player is moving up or down the paddle
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyData == Keys.S)
+            { PongGame.p1movesDown = true; }
+            if (e.KeyData == Keys.W)
+            { PongGame.p1movesUp = true; }
+            if (e.KeyData == Keys.L)
+            { PongGame.p2movesDown = true; }
+            if (e.KeyData == Keys.P)
+            { PongGame.p2movesUp = true; }
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyData == Keys.S)
+            { PongGame.p1movesDown = false; }
+            if (e.KeyData == Keys.W)
+            { PongGame.p1movesUp = false; }
+            if (e.KeyData == Keys.L)
+            { PongGame.p2movesDown = false; }
+            if (e.KeyData == Keys.P)
+            { PongGame.p2movesUp = false; }
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        { PongGame.IncreaseSpeed(); }     //Every 3 seconds, this timer will increase Overall speed
+    }
+}
+
