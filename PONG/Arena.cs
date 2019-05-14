cb38535cbfa6ff00c19c60700edd8b32afc9f1c0
Feriixu <feriixu@users.noreply.github.com>
2019-05-14 16:36:29 +0200

    sachen

---
+++ PONG/Arena.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PONG
+{
+    public partial class Arena : UserControl
+    {
+        public Arena()
+        {
+            InitializeComponent();
+        }
+    }
+}
